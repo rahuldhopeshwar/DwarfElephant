e1d5c1f85091a0504c5221afa021a264266059bb
denise91 <degen@aices.rwth-aachen.de>
2017-03-20 10:24:24 +0100

    New Userobject for Offline Stage

---
+++ include/userobjects/DwarfElephantOfflineStage.h
@@ -0,0 +1,72 @@
+///-------------------------------------------------------------------------
+#ifndef DWARFELEPHANTOFFLINESTAGE_H
+#define DWARFELEPHANTOFFLINESTAGE_H
+
+///---------------------------------INCLUDES--------------------------------
+//libMesh includes
+#include "libmesh/equation_systems.h"
+#include "libmesh/sparse_matrix.h"
+#include "libmesh/petsc_matrix.h"
+#include "libmesh/petsc_vector.h"
+#include "libmesh/getpot.h"
+
+// MOOSE includes
+#include "NodalUserObject.h"
+#include "DisplacedProblem.h"
+#include "MooseMesh.h"
+
+// MOOSE includes (DwarfElephant package)
+#include "DwarfElephantRBClasses.h"
+#include "DwarfElephantRBClassesAssemble.h"
+
+
+///-------------------------------------------------------------------------
+// Forward Declarations
+namespace libMesh
+{
+  class EquationSystems;
+  template <typename T> class SparseMatrix;
+  template <typename T> class PetscMatrix;
+  template <typename T> class PetscVector;
+}
+
+class MooseMesh;
+class DwarfElephantOfflineStage;
+
+template<>
+InputParameters validParams<DwarfElephantOfflineStage>();
+
+class DwarfElephantOfflineStage :
+  public NodalUserObject // Code does not work for GeneralUserObject
+{
+  public:
+    DwarfElephantOfflineStage(const InputParameters & params);
+
+    void offlineStage();
+    void transferAffineOperators(bool _skip_matrix_assembly_in_rb_system, bool _skip_vector_assembly_in_rb_system);
+
+    virtual void initialize() override;
+    virtual void execute() override;
+    virtual void threadJoin(const UserObject & y);
+    virtual void finalize() override;
+
+  protected:
+    bool _use_displaced;
+    bool _skip_matrix_assembly_in_rb_system;
+    bool _skip_vector_assembly_in_rb_system;
+    bool _F_equal_to_output;
+    bool _store_basis_functions;
+
+    std::string _parameters_filename;
+    std::string _system_name;
+
+    const std::set<SubdomainID> & _block_ids;
+
+    EquationSystems & _es;
+    TransientNonlinearImplicitSystem & _sys;
+
+    MooseMesh * _mesh_ptr;
+    DwarfElephantRBConstruction * _rb_con_ptr;
+};
+///-------------------------------------------------------------------------
+#endif // DWARFELEPHANTINITIALIZERBSYSTEM_H
---
+++ src/userobjects/DwarfElephantOfflineStage.C
@@ -0,0 +1,108 @@
+ ///-------------------------------------------------------------------------
+// MOOSE includes (DwarfElephant package)
+#include "DwarfElephantOfflineStage.h"
+
+template<>
+InputParameters validParams<DwarfElephantOfflineStage>()
+{
+  InputParameters params = validParams<NodalUserObject>();
+
+  params.addParam<bool>("use_displaced", false, "Enable/disable the use of the displaced mesh for the data retrieving.");
+  params.addRequiredParam<bool>("store_basis_functions","Determines whether the basis functions are stored or not.");
+  params.addParam<bool>("F_equal_to_output", true, "Determines whether F is equal to the output vector or not.");
+  params.addParam<bool>("skip_matrix_assembly_in_rb_system", true, "Determines whether the matrix is assembled in the RB System or in the nl0 system.");
+  params.addParam<bool>("skip_vector_assembly_in_rb_system", true, "Determines whether the vectors are assembled in the RB System or in the nl0 system.");
+  params.addParam<std::string>("system","nl0","The name of the system that should be read in.");
+
+  return params;
+}
+
+DwarfElephantOfflineStage::DwarfElephantOfflineStage(const InputParameters & params):
+  NodalUserObject(params),
+  _use_displaced(getParam<bool>("use_displaced")),
+  _skip_matrix_assembly_in_rb_system(getParam<bool>("skip_matrix_assembly_in_rb_system")),
+  _skip_vector_assembly_in_rb_system(getParam<bool>("skip_matrix_assembly_in_rb_system")),
+  _F_equal_to_output(getParam<bool>("F_equal_to_output")),
+  _store_basis_functions(getParam<bool>("store_basis_functions")),
+  _system_name(getParam<std::string>("system")),
+  _block_ids(this->blockIDs()),
+  _es(_use_displaced ? _fe_problem.getDisplacedProblem()->es() : _fe_problem.es()),
+  _sys(_es.get_system<TransientNonlinearImplicitSystem>(_system_name)),
+  _mesh_ptr(&_fe_problem.mesh())
+{
+}
+
+void
+DwarfElephantOfflineStage::transferAffineOperators(bool _skip_matrix_assembly_in_rb_system, bool _skip_vector_assembly_in_rb_system)
+{
+  // Transfer the vectors
+  if (_skip_vector_assembly_in_rb_system)
+  {
+    // Transfer the data for the F vectors.
+    for(unsigned int _q=0; _q<_qf; _q++)
+     _rb_con_ptr->get_Fq(_q)->operator=(_sys.get_vector("Re_non_time"));
+//      _rb_con_ptr->get_Fq(_q)->operator=(*_sys.rhs);
+//      _rb_con_ptr->get_Fq(_q)->operator=(_sys.get_vector("Re_non_time"));
+
+    // Transfer the data for the output vectors.
+    if (_F_equal_to_output)
+    {
+      for(unsigned int _q=0; _q<_ql; _q++)
+        _rb_con_ptr->get_output_vector(0,_q)->operator=(_sys.get_vector("Re_non_time"));
+//        _rb_con_ptr->get_output_vector(0,_q)->operator=(*_sys.rhs);
+    }
+    else if (!_F_equal_to_output)
+      mooseError("Currently, the code handles the compliant case, only.");
+  }
+
+  if (_skip_matrix_assembly_in_rb_system)
+  {
+    // The stiffness matrices are transfered in the RBKernel class.
+
+    // Transfer the inner product matrix
+    _rb_con_ptr->get_inner_product_matrix()->close();
+    _rb_con_ptr->get_inner_product_matrix()->add(1,*_sys.matrix);
+  }
+}
+
+void
+DwarfElephantOfflineStage::offlineStage()
+{
+  // This method performs the offline stage of the RB problem.
+
+  // Computation of the reduced basis space.
+//  trainReducedBasis();
+//
+//  // Write the offline data to file (xdr format).
+//  _rb_con_ptr->get_rb_evaluation().legacy_write_offline_data_to_files();
+//
+//  // If desired, store the basis functions (xdr format).
+//  if (_store_basis_functions)
+//  {
+//    _rb_con_ptr->get_rb_evaluation().write_out_basis_functions(*_rb_con_ptr);
+//  }
+//
+//  _rb_con_ptr->print_basis_function_orthogonality();
+}
+
+void
+DwarfElephantOfflineStage::initialize()
+{
+}
+
+void
+DwarfElephantOfflineStage::execute()
+{
+    offlineStage();
+}
+
+void
+DwarfElephantInitializeRBSystem::threadJoin(const UserObject & y)
+{
+}
+
+
+void
+DwarfElephantInitializeRBSystem::finalize()
+{
+}
