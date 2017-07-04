ae822283b0b9cca7e99f13d42f2ed80b24f3be7c
denise91 <degen@aices.rwth-aachen.de>
2017-07-04 14:18:06 +0200

    Initializing RBAssembly class

--- include/base/DwarfElephantRBAssembly.h
+++ include/base/DwarfElephantRBAssembly.h
@@ -1,47 +1,10 @@
 #ifndef DWARFELEPHANTRBASSEMBLY_H
 #define DWARFELEPHANTRBASSEMBLY_H
 
-//#include "MooseArray.h"
-//#include "MooseTypes.h"
 #include "Assembly.h"
 
-//// libMesh
-//#include "libmesh/dense_matrix.h"
-//#include "libmesh/dense_vector.h"
-//#include "libmesh/enum_quadrature_type.h"
-//#include "libmesh/fe_type.h"
-
-//// libMesh forward declarations
-//namespace libMesh
-//{
-//class DofMap;
-//class CouplingMatrix;
-//class Elem;
-//template <typename T>
-//class FEGenericBase;
-//typedef FEGenericBase<Real> FEBase;
-//class Node;
-//template <typename T>
-//class NumericVector;
-//template <typename T>
-//class SparseMatrix;
-//
-//typedef VectorValue<Real> RealVectorValue;
-//typedef RealVectorValue RealGradient;
-//
-//typedef TensorValue<Real> RealTensorValue;
-//typedef RealTensorValue RealTensor;
-//}
-
 // MOOSE Forward Declares
-//class MooseMesh;
-//class ArbitraryQuadrature;
 class SystemBase;
-//class MooseVariable;
-//class XFEMInterface;
-//typedef MooseArray<std::vector<Real>> VariablePhiValue;
-//typedef MooseArray<std::vector<RealGradient>> VariablePhiGradient;
-//typedef MooseArray<std::vector<RealTensor>> VariablePhiSecond;
 class Assembly;
 
 
@@ -51,9 +14,43 @@ public:
   DwarfElephantRBAssembly(SystemBase & sys, THREAD_ID tid);
   virtual ~DwarfElephantRBAssembly();
 
+  void cacheStiffnessMatrixContribution(numeric_index_type i, numeric_index_type j, Real value);
+  void cacheSubdomainStiffnessMatrixContribution(numeric_index_type i, numeric_index_type j, Real value, unsigned int subdomain);
+  void cacheSubdomainMassMatrixContribution(numeric_index_type i, numeric_index_type j, Real value, unsigned int subdomain);
+  void cacheSubdomainResidual(numeric_index_type i, Real value, unsigned int subdomain);
+  void cacheResidual(numeric_index_type i, Real value);
+
+  void setCachedSubdomainResidual(NumericVector<Number> & _residual, unsigned int subdomain);
+  void setCachedResidual(NumericVector<Number> & _residual);
+  void setCachedStiffnessMatrixContributions(SparseMatrix<Number> & _jacobian);
+  void setCachedSubdomainStiffnessMatrixContributions(SparseMatrix<Number> & _jacobian, unsigned int subdomain);
+  void setCachedSubdomainMassMatrixContributions(SparseMatrix<Number> & _mass, unsigned int subdomain);
+
+  void resizeSubdomainStiffnessMatrixCaches(unsigned int subdomains);
+  void resizeSubdomainMassMatrixCaches(unsigned int subdomains);
+  void resizeSubdomainVectorCaches(unsigned int subdomains);
+
 protected:
   SystemBase & _sys;
   THREAD_ID _tid;
+
+  std::vector <numeric_index_type> _cached_jacobian_contribution_rows;
+  std::vector <numeric_index_type> _cached_jacobian_contribution_cols;
+  std::vector <Real> _cached_jacobian_contribution_vals;
+
+  std::vector<std::vector <numeric_index_type>> _cached_jacobian_subdomain_contribution_rows;
+  std::vector<std::vector <numeric_index_type>> _cached_jacobian_subdomain_contribution_cols;
+  std::vector<std::vector <Real>> _cached_jacobian_subdomain_contribution_vals;
+
+  std::vector<std::vector <numeric_index_type>> _cached_mass_subdomain_contribution_rows;
+  std::vector<std::vector <numeric_index_type>> _cached_mass_subdomain_contribution_cols;
+  std::vector<std::vector <Real>> _cached_mass_subdomain_contribution_vals;
+
+  std::vector<std::vector <numeric_index_type>> _cached_residual_subdomain_contribution_rows;
+  std::vector<std::vector <Real>> _cached_residual_subdomain_contribution_vals;
+
+  std::vector <numeric_index_type> _cached_residual_contribution_rows;
+  std::vector <Real> _cached_residual_contribution_vals;
 };
 
 #endif /* DWARFELEPHANTRBASSEMBLY_H */
--- src/base/DwarfElephantRBAssembly.C
+++ src/base/DwarfElephantRBAssembly.C
@@ -14,28 +14,6 @@
 
 #include "DwarfElephantRBAssembly.h"
 
-//// MOOSE includes
-//#include "SubProblem.h"
-//#include "ArbitraryQuadrature.h"
-//#include "SystemBase.h"
-//#include "MooseTypes.h"
-//#include "MooseMesh.h"
-//#include "MooseVariable.h"
-//#include "MooseVariableScalar.h"
-//#include "XFEMInterface.h"
-//
-//// libMesh
-//#include "libmesh/coupling_matrix.h"
-//#include "libmesh/dof_map.h"
-//#include "libmesh/elem.h"
-//#include "libmesh/equation_systems.h"
-//#include "libmesh/fe_interface.h"
-//#include "libmesh/node.h"
-//#include "libmesh/quadrature_gauss.h"
-//#include "libmesh/sparse_matrix.h"
-//#include "libmesh/tensor_value.h"
-//#include "libmesh/vector_value.h"
-
 DwarfElephantRBAssembly::DwarfElephantRBAssembly(SystemBase & sys, THREAD_ID tid)
   : Assembly(sys, tid),
     _sys(sys),
@@ -45,4 +23,123 @@ DwarfElephantRBAssembly::DwarfElephantRBAssembly(SystemBase & sys, THREAD_ID tid
 
 DwarfElephantRBAssembly::~DwarfElephantRBAssembly()
 {
+}
+
+void
+DwarfElephantRBAssembly::cacheSubdomainResidual(numeric_index_type i, Real value, unsigned int subdomain)
+{
+  _cached_residual_subdomain_contribution_rows[subdomain].push_back(i);
+  _cached_residual_subdomain_contribution_vals[subdomain].push_back(value);
+}
+
+void
+DwarfElephantRBAssembly::cacheResidual(numeric_index_type i, Real value)
+{
+  _cached_residual_contribution_rows.push_back(i);
+  _cached_residual_contribution_vals.push_back(value);
+}
+
+void
+DwarfElephantRBAssembly::setCachedSubdomainResidual(NumericVector<Number> & _residual, unsigned int subdomain)
+{
+  _residual.close();
+
+  for (unsigned int i = 0; i < _cached_residual_subdomain_contribution_vals[subdomain].size(); ++i)
+    _residual.set(_cached_residual_subdomain_contribution_rows[subdomain][i], _cached_residual_subdomain_contribution_vals[subdomain][i]);
+}
+
+void
+DwarfElephantRBAssembly::setCachedResidual(NumericVector<Number> & _residual)
+{
+  _residual.close();
+
+  for (unsigned int i = 0; i < _cached_residual_contribution_vals.size(); ++i)
+    _residual.set(_cached_residual_contribution_rows[i], _cached_residual_contribution_vals[i]); // / 0.07778);
+}
+
+void
+DwarfElephantRBAssembly::cacheStiffnessMatrixContribution(numeric_index_type i, numeric_index_type j, Real value)
+{
+  _cached_jacobian_contribution_rows.push_back(i);
+  _cached_jacobian_contribution_cols.push_back(j);
+  _cached_jacobian_contribution_vals.push_back(value);
+}
+
+void
+DwarfElephantRBAssembly::resizeSubdomainStiffnessMatrixCaches(unsigned int subdomains)
+{
+  _cached_jacobian_subdomain_contribution_rows.resize(subdomains);
+  _cached_jacobian_subdomain_contribution_cols.resize(subdomains);
+  _cached_jacobian_subdomain_contribution_vals.resize(subdomains);
+}
+
+void
+DwarfElephantRBAssembly::resizeSubdomainMassMatrixCaches(unsigned int subdomains)
+{
+  _cached_mass_subdomain_contribution_rows.resize(subdomains);
+  _cached_mass_subdomain_contribution_cols.resize(subdomains);
+  _cached_mass_subdomain_contribution_vals.resize(subdomains);
+}
+
+void
+DwarfElephantRBAssembly::resizeSubdomainVectorCaches(unsigned int subdomains)
+{
+  _cached_residual_subdomain_contribution_rows.resize(subdomains);
+  _cached_residual_subdomain_contribution_vals.resize(subdomains);
+}
+
+void
+DwarfElephantRBAssembly::cacheSubdomainStiffnessMatrixContribution(numeric_index_type i, numeric_index_type j, Real value, unsigned int subdomain)
+{
+  _cached_jacobian_subdomain_contribution_rows[subdomain].push_back(i);
+  _cached_jacobian_subdomain_contribution_cols[subdomain].push_back(j);
+  _cached_jacobian_subdomain_contribution_vals[subdomain].push_back(value);
+}
+
+void
+DwarfElephantRBAssembly::cacheSubdomainMassMatrixContribution(numeric_index_type i, numeric_index_type j, Real value, unsigned int subdomain)
+{
+  _cached_mass_subdomain_contribution_rows[subdomain].push_back(i);
+  _cached_mass_subdomain_contribution_cols[subdomain].push_back(j);
+  _cached_mass_subdomain_contribution_vals[subdomain].push_back(value);
+}
+
+void
+DwarfElephantRBAssembly::setCachedStiffnessMatrixContributions(SparseMatrix<Number> & _jacobian)
+{
+  _jacobian.close();
+  _jacobian.zero_rows(_cached_jacobian_contribution_rows);
+
+  for (unsigned int i = 0; i < _cached_jacobian_contribution_vals.size(); ++i)
+    _jacobian.set(_cached_jacobian_contribution_rows[i],
+                  _cached_jacobian_contribution_cols[i],
+                  _cached_jacobian_contribution_vals[i]);
+}
+
+void
+DwarfElephantRBAssembly::setCachedSubdomainStiffnessMatrixContributions(SparseMatrix<Number> & _jacobian, unsigned int subdomain)
+{
+  _jacobian.close();
+  _jacobian.zero_rows(_cached_jacobian_subdomain_contribution_rows[subdomain]);
+
+  for (unsigned int i = 0; i < _cached_jacobian_subdomain_contribution_vals[subdomain].size(); ++i)
+  {
+    _jacobian.set(_cached_jacobian_subdomain_contribution_rows[subdomain][i],
+                  _cached_jacobian_subdomain_contribution_cols[subdomain][i],
+                  _cached_jacobian_subdomain_contribution_vals[subdomain][i]);
+  }
+}
+
+void
+DwarfElephantRBAssembly::setCachedSubdomainMassMatrixContributions(SparseMatrix<Number> & _mass, unsigned int subdomain)
+{
+  _mass.close();
+  _mass.zero_rows(_cached_mass_subdomain_contribution_rows[subdomain]);
+
+  for (unsigned int i = 0; i < _cached_mass_subdomain_contribution_vals[subdomain].size(); ++i)
+  {
+    _mass.set(_cached_mass_subdomain_contribution_rows[subdomain][i],
+              _cached_mass_subdomain_contribution_cols[subdomain][i],
+              _cached_mass_subdomain_contribution_vals[subdomain][i]);
+  }
 }
